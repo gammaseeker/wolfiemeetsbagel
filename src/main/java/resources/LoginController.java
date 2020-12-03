08a15f431193297e956e3457071997153b6d2b31
siyuan zou <siyuanzou99@gmail.com>
2020-12-03 14:21:58 -0500

    added print test for getEmployeeID

--- src/main/java/resources/LoginController.java
+++ src/main/java/resources/LoginController.java
@@ -53,13 +53,15 @@ public class LoginController extends HttpServlet {
 			if(role.equals("manager")) {
 				EmployeeDao employeeDao = new EmployeeDao();
 				String employeeID = employeeDao.getEmployeeID(username);
-				request.getSession(true).setAttribute("employeeID", employeeID);				
+				request.getSession(true).setAttribute("employeeID", employeeID);
+				System.out.println(employeeID);
 				response.sendRedirect("managerHome.jsp");
 			}
 			else if(role.equals("customerRepresentative")) {
 				EmployeeDao employeeDao = new EmployeeDao();
 				String employeeID = employeeDao.getEmployeeID(username);
-				request.getSession(true).setAttribute("employeeID", employeeID);				
+				request.getSession(true).setAttribute("employeeID", employeeID);		
+				System.out.println(employeeID);
 				response.sendRedirect("customerRepresentativeHome.jsp");
 			}
 			else {
